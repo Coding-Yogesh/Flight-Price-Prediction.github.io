4af89d7bbfd1c5d2cf7485eb999e34b35cd59b79
coding-yogesh <126610369+coding-yogesh@users.noreply.github.com>
2023-04-19 16:51:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/predict", methods=["GET", "POST"])
+@app.route("/predict", methods=["POST"])
 @cross_origin()
 def predict():
     if request.method == "POST":
